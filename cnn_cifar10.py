82d4b7d4e6a0e64e9708f0b93d20eedf5641ad8a
asahi417 <spacefunkspacefunk@gmail.com>
2020-01-15 01:08:37 +0900

    tweak confi

--- cnn_cifar10.py
+++ cnn_cifar10.py
@@ -185,7 +185,6 @@ class Net(nn.Module):
         """ Network Architecture """
         super(Net, self).__init__()
         # components
-        self.__logger = create_log()
         self.pool = nn.MaxPool2d(2, 2)
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -310,6 +309,7 @@ class ConvNet:
         for i, data in enumerate(data_loader, 1):
             # get the inputs (data is a list of [inputs, labels])
             inputs, labels = data
+            print(inputs.shape, labels.shape)
             if self.if_use_gpu:
                 inputs, labels = inputs.cuda(), labels.cuda()
             # zero the parameter gradients
